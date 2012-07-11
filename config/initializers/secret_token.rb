6362bce2516342f02a8a50b714101525b402a0b5
Nisarg Shah <nisargshah100@gmail.com>
2012-07-11 12:31:32 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharityChain::Application.config.secret_token = '376e9126b58d09fd292a5baab3ab727dc5979204d11964a6503b8997a9d959a87eaa2585f4571b3ae9e93a18caa5719f1bc5c676744acf2ec3b24ea2f16b85bf'
