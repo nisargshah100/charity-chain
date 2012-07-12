9286c839d32363c9f20bc73f901713ed3821bde1
Nisarg Shah <nisargshah100@gmail.com>
2012-07-12 17:04:52 -0400

    hooked up

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,5 +1,4 @@
 class GoalsController < ApplicationController
   def show
-    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 CharityChain::Application.routes.draw do
   resources :goals, only: [:show]
   root :to => 'pages#index'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
