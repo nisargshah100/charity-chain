c0c24c8ff9b38c860eadb7b0877445faf9e4ee7c
Conan Rimmer <conan@livingsocial.com>
2012-07-18 16:29:06 -0400

    spruce up welcome page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :authentication_token
   has_many :goals
   before_create :ensure_authentication_token
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
+  validates :password, presence: true
 
   def ensure_authentication_token
     self.authentication_token = rand(72**8).to_s(36)
