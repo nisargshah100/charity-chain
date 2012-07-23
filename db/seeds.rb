474610a1bc415bf689e05d3fc8f2b435d2aeea9b
Conan Rimmer <conan@livingsocial.com>
2012-07-23 17:59:24 -0400

    db seeds without check-ins

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+amounts     = [1000,2500,5000]
+card_types  = ["Visa","Mastercard","Discover","Amex"]
+
+User.destroy_all
+Goal.destroy_all
+Scheduler.destroy_all
+Contribution.destroy_all
+Payment.destroy_all
+
+require 'timecop'
+require 'faker'
+
+Timecop.travel(15.days.ago)
+test_user = User.create email: "tester@lsqa.net",
+                        password: "password"
+goal      = test_user.goals.create name: "Walk a mile a day"
+schedule  = Scheduler.create goal: goal,
+                            sunday: true,
+                            monday: true,
+                            tuesday: true,
+                            wednesday: true,
+                            thursday: true,
+                            friday: true,
+                            saturday: true
+4.times do
+  contribution = goal.contributions.create name: Faker::Name.name
+  payment = Payment.create  contribution: contribution,
+                            amount: amounts[rand(0..amounts.size-1)],
+                            last_four: rand(1..9999).to_s.rjust(4, '0'),
+                            card_type: card_types[rand(0..amounts.size-1)]
+  contribution.update_attribute :payment, payment
+  goal.add_to_reserve_amount payment.amount
+end
+
+14.times do
+  Timecop.travel(1.day.from_now)
+  # check-in
+end
+
+Timecop.return
