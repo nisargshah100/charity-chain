ee853f836ec3006c616ff6dfdba035ed2a8bc127
Nisarg Shah <nisargshah100@gmail.com>
2012-07-24 19:38:02 -0400

    goal controller testing

--- spec/controllers/api/v1/goal_controller_spec.rb
+++ spec/controllers/api/v1/goal_controller_spec.rb
@@ -1,25 +1,43 @@
 require 'spec_helper'
 
-describe Api::V1::GoalsController do
+describe Api::V1::GoalsController, :type => :controller do
 
   let!(:user) { User.create(:email => 'one@one.com', :password => 'oneone', :name => 'One Man')}
+  let!(:my_goal) { Goal.create(:name => 'Goal 1', :user => user) }
+  let!(:goal) { Goal.create(:name => 'Goal 2') }
+
+  let!(:schedulers) { [my_goal, goal].each { |g| Scheduler.create(:goal => g) }}
 
   context 'unauthenticated' do
     it 'returns false' do
       visit api_v1_goals_path
       page.should have_content('false')
     end
+
+    it 'creates a goal' do
+      visit api_v1_goals_path
+      page.should have_content('false')
+    end
   end
 
   context 'authenticated' do
-    # before(:each) do
-    #   # login_user('one@one.com', 'oneone')
-    #   post sessions_path, { email: 'one@one.com', password: 'oneone' }
-    # end
+    context '#index' do
+      before(:each) do
+        visit api_v1_goals_path(:token => user.authentication_token)
+      end
 
-    # it 'returns the goals' do
-    #   visit api_v1_goals_path
-    #   page.should have_content('Goal 1')
-    # end
+      it 'returns empty goals' do
+        page.should have_content('[]')
+      end
+
+      it 'returns my goals' do
+        page.should have_content('Goal 1')
+        page.should_not have_content('Goal 2')
+      end
+    end
+
+    context '#create' do
+      pending 'to do!'
+    end
   end
 end
