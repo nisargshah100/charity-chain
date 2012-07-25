b62366194dc7ea028fc51c2a6c463ae70e83e5ee
Conan Rimmer <conan@livingsocial.com>
2012-07-25 14:25:03 -0400

    clean up session create to make logic righter and less confusing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
   def create
+    logout # kill an existing auth if present
     user = login params[:email], params[:password]
-    render :json => { logged_in: !user.nil? }
+    render :json => { logged_in: logged_in? }
   end
   
   def destroy
     logout
-    redirect_to root_url, notice: "Logged out"
+    redirect_to root_path, notice: "Logged out"
   end
 end
