48f5e201262d7000a818013dca7a347dc7e14cfe
Chris Maddox <tyre77@gmail.com>
2012-07-25 10:10:16 -0400

    fixing up Goal#streaks

--- app/models/scheduler.rb
+++ app/models/scheduler.rb
@@ -7,7 +7,7 @@ class Scheduler < ActiveRecord::Base
   # Example:
   #  scheduler.days
   #  #=> [0,3,5]
-  # 
+  #
   # @return [Array] kind A collection of integers representing the weekdays, e.g. (Monday is 1, Tuesday is 2)
 
   def days
@@ -23,7 +23,7 @@ class Scheduler < ActiveRecord::Base
   # Example:
   #  scheduler.generate_dates("21/6/1988")
   #  #=> ["20/6/1988", "18/6/1988", "16/6/1988"]
-  # 
+  #
   # @param [DateTime] end_date The last date in the generated set of dates
   # @return [Array] kind A collection of dates that range from the first check-in to the end_date specified.
 
@@ -39,7 +39,7 @@ class Scheduler < ActiveRecord::Base
         date if days.include?(date.wday)
       end
     end
-
+    dates.unshift DateHelper.date if goal.checked_in_today?
     dates
   end
 end
