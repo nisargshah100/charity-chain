fd14df2ae466218f824d30d487dc02ce2c6b177c
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-07-25 11:52:33 -0400

    Remove extraneous variable

--- app/controllers/api/v1/check_ins_controller.rb
+++ app/controllers/api/v1/check_ins_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::CheckInsController < Api::ApiController
   def create
     goal = current_user.goals.find_by_id(params[:goal_id])
     if goal
-      check_in = goal.check_ins.create(params[:check_in])
+      goal.check_ins.create(params[:check_in])
       render :json => true
     else
       render :json => false
